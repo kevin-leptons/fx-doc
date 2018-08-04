2520971a603b83880dae479a7f9ec40fe2f9dfb1
Kevin Leptons <kevin.leptons@gmail.com>
2018-08-04 10:21:48 +0700

    up pip v0.10.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('readme') as f:
 
 setup(
     name='fx-doc',
-    version='0.9.0',
+    version='0.10.0',
     license='MIT',
     description='Build reStructuredText to HTML, PDF and text',
     long_description=long_desc,
